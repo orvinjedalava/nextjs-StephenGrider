c1c4c27e8b3187977f4d3bcc7b379b3174bcb28b
orvinjedalava <ojedbub@gmail.com>
2025-08-29 12:13:46 +1000

    Section 6 - 56

--- Project-002/snippets/src/app/snippets/[id]/page.tsx
+++ Project-002/snippets/src/app/snippets/[id]/page.tsx
@@ -44,3 +44,14 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     </div>
   );
 }
+
+// Implements caching in dynamic routes
+// Only activated for production environments
+export async function generateStaticParams() {
+  const snippets = await db.snippet.findMany();
+  return snippets.map((snippet) => {
+    return {
+      id: snippet.id.toString(),
+    };
+  });
+}
