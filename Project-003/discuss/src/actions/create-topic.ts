1169d5bb16ac2b464f26d18d42c17f47e930e5c5
orvinjedalava <ojedbub@gmail.com>
2025-08-29 22:51:06 +1000

    Section 7 - 86

--- Project-003/discuss/src/actions/create-topic.ts
+++ Project-003/discuss/src/actions/create-topic.ts
@@ -14,15 +14,31 @@ const createTopicSchema = z.object({
     .min(10),
 });
 
-export async function createTopic(formData: FormData) {
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  }
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
   const result = createTopicSchema.safeParse({
     name: formData.get('name'),
     description: formData.get('description')
   });
 
   if (!result.success) {
-    console.log(result.error.flatten().fieldErrors);
+    return {
+      errors: result.error.flatten().fieldErrors
+    }
   }
 
   // TODO: revalidate the homepage
+
+  return {
+    errors: {}
+  };
 }
--- Project-003/discuss/src/components/topics/topic-create-form.tsx
+++ Project-003/discuss/src/components/topics/topic-create-form.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useActionState } from 'react';
 import {
   Input,
   Button,
@@ -9,13 +12,17 @@ import {
 import * as actions from '@/actions';
 
 export default function TopicCreateForm() {
+  const [formState, action] = useActionState(actions.createTopic, {
+    errors: {}
+  });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
             <Input
