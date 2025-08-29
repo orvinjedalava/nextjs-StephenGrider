10db815e672afbd0cfd226a1d3383bd9189ada1e
orvinjedalava <ojedbub@gmail.com>
2025-08-29 14:59:20 +1000

    Section 7 - 67

--- Project-003/discuss/src/app/providers.tsx
+++ Project-003/discuss/src/app/providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
+import { SessionProvider } from 'next-auth/react';
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -8,5 +9,9 @@ interface ProvidersProps {
 
 export default function Providers({ children }: ProvidersProps) {
   // NextUIProvider sets the react context that all NextUI Components use across the application
-  return (<NextUIProvider>{children}</NextUIProvider>);
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  );
 }
