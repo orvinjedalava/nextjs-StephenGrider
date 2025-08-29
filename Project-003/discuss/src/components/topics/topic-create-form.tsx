4c4ed66b1db6813b152cb1ef6724270d65c7b29e
orvinjedalava <ojedbub@gmail.com>
2025-08-29 22:58:53 +1000

    Section 7 - 88

--- Project-003/discuss/src/components/topics/topic-create-form.tsx
+++ Project-003/discuss/src/components/topics/topic-create-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useActionState } from 'react';
+import { useActionState, startTransition } from 'react';
 import {
   Input,
   Button,
@@ -16,26 +16,38 @@ export default function TopicCreateForm() {
     errors: {}
   });
 
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    startTransition(() => {
+      action(formData);
+    });
+  }
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={action}>
+        <form onSubmit={handleSubmit} noValidate>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
             <Input
               name="name" 
               label="Name" 
               labelPlacement="outside" 
-              placeholder="Name" 
+              placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(', ')} 
             />
             <Textarea
               name="description" 
               label="Description" 
               labelPlacement="outside" 
               placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(', ')}
             />
             <Button type="submit">Submit</Button>
           </div>
