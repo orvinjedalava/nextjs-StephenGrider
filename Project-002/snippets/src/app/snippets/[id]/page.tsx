98ceeecd058b9355a87c411daf4d86d8226e6010
orvinjedalava <ojedbub@gmail.com>
2025-08-29 10:09:05 +1000

    Section 4 - 43

--- Project-002/snippets/src/app/snippets/[id]/page.tsx
+++ Project-002/snippets/src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { db } from '@/db';
+import * as actions from '@/actions';
 
 interface SnippetShowPageProps {
   params: Promise<{
@@ -18,6 +19,8 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
   if (!snippet) {
     return notFound();
   }
+
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
   
   return (
     <div>
@@ -30,7 +33,9 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
           >
               Edit
           </Link>
-          <button className="p-2 border rounded border-gray-300">Delete</button>
+          <form action={deleteSnippetAction}>
+            <button className="p-2 border rounded border-gray-300">Delete</button>
+          </form>
         </div>
       </div>
       <pre className="p-3 border rounded bg-gray-200 border-gray-200">
