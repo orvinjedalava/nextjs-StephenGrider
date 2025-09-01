4ad998b32602a40b2c1e8122ea2a766f209258a5
orvinjedalava <ojedbub@gmail.com>
2025-09-01 10:52:33 +1000

    Section 8 - 105

--- Project-003/discuss/next.config.ts
+++ Project-003/discuss/next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
