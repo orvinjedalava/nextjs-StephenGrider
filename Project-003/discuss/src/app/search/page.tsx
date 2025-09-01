9f4e0b8a6ece75157abc613ab7a7f73aef08a533
orvinjedalava <ojedbub@gmail.com>
2025-09-01 13:43:00 +1000

    Section 10 - 128

---
+++ Project-003/discuss/src/app/search/page.tsx
@@ -0,0 +1,17 @@
+import { redirect } from 'next/navigation';
+
+interface SearchPageProps {
+  searchParams: Promise<{
+    term: string;
+  }>;
+}
+
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const { term } = await searchParams;
+
+  if (!term) {
+    redirect('/');
+  }
+
+  return <div>{term}</div>
+}
