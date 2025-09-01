0f5701d5fd2fb212380e557560186d0274959d9b
orvinjedalava <ojedbub@gmail.com>
2025-09-01 10:08:03 +1000

    Section 8 - 97

--- Project-003/discuss/src/app/topics/[slug]/page.tsx
+++ Project-003/discuss/src/app/topics/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import PostCreateForm from "@/components/posts/post-create-form";
+
 interface TopicShowPageProps {
   params: Promise<{
     slug: string;
@@ -14,7 +16,7 @@ export default async function TopicShowPage({ params }: TopicShowPageProps) {
     </div>
 
     <div>
-      
+      <PostCreateForm />
     </div>
   </div>
 }
